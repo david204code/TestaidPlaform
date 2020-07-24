f8d0b341c574ebe3b108a276569ee8ea5e0b9e81
David Wu <david204code@gmail.com>
2020-07-24 16:55:19 +0100

    has_many :accepted_helps ONTO HelpSerializer

--- app/serializers/help_serializer.rb
+++ app/serializers/help_serializer.rb
@@ -1,4 +1,6 @@
 class HelpSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :description, :request_type, :location_long, :location_lat, :status, :color, :user_id
+
+  has_many :accepted_helps
 end
