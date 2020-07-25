4ed18f2cf8b8deaaae3ee37244cb9a6927e4d7fb
David Wu <david204code@gmail.com>
2020-07-25 16:12:48 +0100

    import axios and using useEffect

--- app/javascript/components/Helps/Helps.jsx
+++ app/javascript/components/Helps/Helps.jsx
@@ -1,7 +1,15 @@
 
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Helps = () => {
+  const [Helps, setHelps] = useState([])
+
+  useEffect(() => {
+    //get all helps from api
+    //update helps in our state
+  })
+
   return <div>This is the Helps#index view for our app.</div>
 }
 
