80fa7dc2357749a41f5d53ba05dfc7f913701ca9
David Wu <david204code@gmail.com>
2020-07-25 16:48:18 +0100

    Getting the data from the api and using useEffect

--- app/javascript/components/Helps/Helps.jsx
+++ app/javascript/components/Helps/Helps.jsx
@@ -1,16 +1,37 @@
 
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 
 const Helps = () => {
-  const [Helps, setHelps] = useState([])
+  const [helps, setHelps] = useState([])
 
   useEffect(() => {
     //get all helps from api
     //update helps in our state
+    axios.get('http://localhost:3000/helps1.json')
+    .then( resp => {
+      // console.log(resp.data.data)
+      setHelps(resp.data.data)
+    })
+    .catch( resp => console.log(resp) )
+  }, [helps.length])
+
+  const list = helps.map( item => {
+    console.log(item.attributes)
+    return (<li key={item.id} attributes ={item.attributes}></li>)
   })
 
-  return <div>This is the Helps#index view for our app.</div>
+  return (
+    <div className ="home">
+      <div className ="header">
+        <h1>Helps</h1>
+        <div className ="subheader">Help App</div>
+      </div>
+      <div className ="grid">
+        {/* <ul>{list}DAvid</ul> */}
+      </div>
+    </div>
+  )
 }
 
 export default Helps; 
