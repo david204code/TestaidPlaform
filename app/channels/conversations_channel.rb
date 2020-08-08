715bc12f5487b678ecf77072c9583fcd69062dc1
David Wu <david204code@gmail.com>
2020-08-08 23:30:14 +0100

    rails generate channel conversations AND add the methods

---
+++ app/channels/conversations_channel.rb
@@ -0,0 +1,13 @@
+class ConversationsChannel < ApplicationCable::Channel
+  def subscribed
+    @conversation = Conversation.find_by(id: params[:conversation])
+    stream_for @conversation
+  end
+
+  def received(data)
+    ConversationsChannel.broadcast_to(@conversation, {conversation: @conversation, users: @conversation.users, messages: @conversation.messages})
+  end
+
+  def unsubscribed
+  end
+end
--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,11 +1,11 @@
-class MessageChannel < ApplicationCable::Channel
-  def subscribed
-    # stream_from "some_channel"
-    conversation = Conversation.find(params[:conversation])
-    stream_for conversation
-  end
+# class MessageChannel < ApplicationCable::Channel
+#   def subscribed
+#     # stream_from "some_channel"
+#     conversation = Conversation.find(params[:conversation])
+#     stream_for conversation
+#   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
+#   def unsubscribed
+#     # Any cleanup needed when channel is unsubscribed
+#   end
+# end
