6ce216b812322aa446c3dff7756dcb1626c01933
David Wu <david204code@gmail.com>
2020-09-22 22:03:51 +0100

    rails active_storage:install and db:migarte

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_221707) do
+ActiveRecord::Schema.define(version: 2020_09_22_205846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,27 @@ ActiveRecord::Schema.define(version: 2020_07_07_221707) do
     t.bigint "user_id"
     t.index ["help_id"], name: "index_accepted_helps_on_help_id"
     t.index ["user_id"], name: "index_accepted_helps_on_user_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "conversations", force: :cascade do |t|
@@ -66,6 +87,7 @@ ActiveRecord::Schema.define(version: 2020_07_07_221707) do
 
   add_foreign_key "accepted_helps", "helps"
   add_foreign_key "accepted_helps", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "conversations", "accepted_helps"
   add_foreign_key "helps", "users"
   add_foreign_key "messages", "conversations"
